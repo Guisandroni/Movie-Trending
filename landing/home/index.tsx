b3d9d9fdefff436a7ff7ef65d70c7243b2395db5
guisandroni <gui.sandroni@gmail.com>
2025-11-18 21:31:57 -0300

    feat: component treanding top movies

--- landing/home/index.tsx
+++ landing/home/index.tsx
@@ -4,7 +4,11 @@ import Header from '@/components/header';
 import MovieCard from '@/components/movieCard';
 import Search from '@/components/search';
 import Spinner from '@/components/spinner';
-import { getMovies, updateSearchCount } from '@/lib/api';
+import {
+  getGlobalTrendingMovies,
+  getMovies,
+  updateSearchCount
+} from '@/lib/api';
 import { useEffect, useState } from 'react';
 
 export function IndexHome() {
@@ -13,7 +17,10 @@ export function IndexHome() {
   const [errorMessage, setErrorMessage] = useState('');
   const [movieList, setMovieList] = useState<Movie[]>([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [trendingMovies, setTrendingMovies] = useState<TrendingItem[]>([]);
+  const [searchTrendingMovies, setSearchTrendingMovies] = useState<
+    TrendingItem[]
+  >([]);
+  const [globalTrendingMovies, setGlobalTrendingMovies] = useState<Movie[]>([]);
 
   useEffect(() => {
     const handle = setTimeout(() => {
@@ -54,12 +61,14 @@ export function IndexHome() {
   useEffect(() => {
     const loadTrending = async () => {
       try {
-        const res = await fetch('/api/trending');
-        // if (!res.ok) return;
-        const data = await res.json();
+        const resLocal = await fetch('/api/trending');
+        if (resLocal.ok) {
+          const dataLocal = await resLocal.json();
+          setSearchTrendingMovies(dataLocal);
+        }
 
-        console.log('teste front api', data);
-        setTrendingMovies(data);
+        const globalMovies = await getGlobalTrendingMovies();
+        setGlobalTrendingMovies(globalMovies.slice(0, 10));
       } catch (error) {
         console.error(error);
       }
@@ -77,33 +86,68 @@ export function IndexHome() {
 
         <section className="all-movies">
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-
-          {trendingMovies.length > 0 && (
-            <section className="trending">
-              <h2>Trending Movies</h2>
-
-              <ul>
-                {trendingMovies.map((movie, index) => (
-                  <li key={movie.id}>
-                    <p>{index + 1}</p>
-                    <img src={movie.posterPath} alt={movie.searchTerm} />
-                    <p className="text-center text-xs mt-1 capitalize text-gray-200">
-                      {movie.searchTerm}
-                    </p>
+          {globalTrendingMovies.length > 0 && !searchTerm && (
+            <section className="  mb-10">
+              <h2 className="text-xl font-bold mb-4">
+                {' '}
+                On the rise in the world
+              </h2>
+              <ul className="flex gap-5 overflow-x-auto pb-4 hide-scrollbar overflow-y-auto ">
+                {globalTrendingMovies.map((movie) => (
+                  <li key={movie.id} className="min-w-[150px]">
+                    <div className="movie-card bg-dark-100 p-2 rounded-xl">
+                      <img
+                        src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                        alt={movie.title}
+                        className="w-full h-auto rounded-lg"
+                      />
+                      <h3 className="text-sm font-bold mt-2 line-clamp-1 text-white">
+                        {movie.title}
+                      </h3>
+                      <div className="flex items-center gap-1 mt-1">
+                        <img src="/star.svg" className="size-3" alt="star" />
+                        <p className="text-xs text-white font-bold">
+                          {movie.vote_average?.toFixed(1)}
+                        </p>
+                      </div>
+                    </div>
                   </li>
                 ))}
               </ul>
             </section>
           )}
+          {!searchTerm && (
+            <>
+              {searchTrendingMovies.length > 0 && (
+                <section className=" trending mb-10">
+                  <h2 className="text-xl font-bold mb-4">
+                    Most Searched Movies{' '}
+                  </h2>
 
-          <h2 className="mt-[4px]">All Movies</h2>
-
+                  <ul className="flex">
+                    {searchTrendingMovies.map((movie, index) => (
+                      <li key={movie.id} className="gap-6">
+                        <p>{index + 1}</p>
+                        <div className=" relative z-10">
+                          <img src={movie.posterPath} alt={movie.searchTerm} />
+                          <p>{movie.searchTerm}</p>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </section>
+              )}
+            </>
+          )}
+          <h2 className="mt-[4px]">
+            {searchTerm ? `Search Filter:  ${searchTerm}` : 'All Movies'}
+          </h2>
           {isLoading ? (
             <Spinner />
           ) : errorMessage ? (
             <p className="text-red-500">{errorMessage}</p>
           ) : (
-            <ul>
+            <ul className="all-movies">
               {movieList.map((movie) => (
                 <MovieCard key={movie.id} movie={movie}></MovieCard>
               ))}
