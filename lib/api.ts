94b3e42978c2aea565acdf2d874f277990e4b678
guisandroni <gui.sandroni@gmail.com>
2025-11-18 21:32:20 -0300

    feat: add push api treanding movies world

--- lib/api.ts
+++ lib/api.ts
@@ -51,3 +51,18 @@ export const updateSearchCount = async (query: string, movie: Movie) => {
 };
 
 
+export const getGlobalTrendingMovies = async (): Promise<Movie[]> => {
+  const endpoint = `${API_BASE_URL}/trending/movie/day?language=en-US`
+  
+  const response = await fetch(endpoint, API_OPTIONS);
+  
+  if(!response.ok){
+    throw new Error('failed to loading trending')
+  }
+  
+  
+  const data = await response.json();
+  
+  return data.results || []
+
+}
